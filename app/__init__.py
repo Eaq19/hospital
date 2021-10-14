218761095e540ba53b6ac0e00c74273eec1eac89
FelipeHerman <j_felipe05@hotmail.com>
2021-10-14 13:05:15 -0500

    Update __init__.py
    Actualizando los blueprints en el archivo que ejecuta el proyecto.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_login import LoginManager
 
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    
+
+    login_manager.init_app(app)
+    app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+
     # Registro de los Blueprints
-    from .inicio import inicio_bp
-    app.register_blueprint(inicio_bp)
-    
+    from .inicio import inicio_blueprints
+    app.register_blueprint(inicio_blueprints)
+
+    from .autenticacion import autenticacion_blueprints
+    app.register_blueprint(autenticacion_blueprints)
+
     return app
