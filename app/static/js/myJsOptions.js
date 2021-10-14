b764f684ff27cf4f5caa46ce16527ebb44105203
FelipeHerman <j_felipe05@hotmail.com>
2021-10-14 12:51:56 -0500

    Añadiendo JS
    Implementacion de JS

---
+++ app/static/js/myJsOptions.js
@@ -0,0 +1,13 @@
+/* Selects */
+document.addEventListener('DOMContentLoaded', function() {
+var elems = document.querySelectorAll('select');
+var instances = M.FormSelect.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+$('select').formSelect();
+});
+
+/* fin Selects */
