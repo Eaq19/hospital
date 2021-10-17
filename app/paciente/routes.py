1bd516ba4fd0817be372d233a9622b08f684f2cc
FelipeHerman <j_felipe05@hotmail.com>
2021-10-17 01:53:12 -0500

    Vistas y Rutas paciente
    Vistas y Rutas del paciente

--- app/paciente/routes.py
+++ app/paciente/routes.py
@@ -1,4 +1,4 @@
-from flask import  render_template
+from flask import render_template
 
 from . import paciente_blueprints
 
@@ -6,3 +6,13 @@ from . import paciente_blueprints
 @paciente_blueprints.route("/paciente", methods=['GET', 'POST'])
 def paciente_index():
     return render_template('paciente_index.html')
+
+
+@paciente_blueprints.route("/citasPaciente", methods=['GET', 'POST'])
+def citas_paciente():
+    return render_template('citas_paciente.html')
+
+
+@paciente_blueprints.route("/comentariosPaciente", methods=['GET', 'POST'])
+def comentarios_paciente():
+    return render_template('comentarios_paciente.html')
