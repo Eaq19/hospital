40defbd8d38972e2d02a7f764ac92e49ce143042
FelipeHerman <j_felipe05@hotmail.com>
2021-10-24 01:45:00 -0500

    lista de usuarios
    Creando la tabla para la lista de usuarios

--- app/administrador/routes.py
+++ app/administrador/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, request
 
+from app.autenticacion.models import User
 from . import administrador_blueprints
 
 
@@ -8,16 +9,25 @@ def administrador_index():
     return render_template('administrador_index.html')
 
 
+# lista de usuarios
+ROWS_PER_PAGE = 5
+
+@administrador_blueprints.route("/usuarios", methods=['GET', 'POST'])
+def usuarios_administrador():
+    page = request.args.get('page', 1, type=int)
+    
+    users = User.query.paginate(page=page, per_page=ROWS_PER_PAGE, error_out=False)
+    
+    return render_template('usuarios_administrador.html', users=users.items)
+
+
+# lista de citas
 @administrador_blueprints.route("/citas", methods=['GET', 'POST'])
 def citas_administrador():
     return render_template('citas_administrador.html')
 
 
+# lista de comentarios
 @administrador_blueprints.route("/comentarios", methods=['GET', 'POST'])
 def comentarios_administrador():
     return render_template('comentarios_administrador.html')
-
-
-@administrador_blueprints.route("/usuarios", methods=['GET', 'POST'])
-def usuarios_administrador():
-    return render_template('usuarios_administrador.html')
